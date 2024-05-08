a7e8472d2994e6ab5a1226c27a35f4cf52674598
tungboio234 <donandniconam1311@outlook.com>
2024-05-09 01:04:03 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,9 +1,1 @@
-function groupAnagrams(strs) {
-  const map = new Map();
-  for (const str of strs) {
-    const sortedStr = str.split("").sort().join("");
-    if (!map.has(sortedStr)) map.set(sortedStr, []);
-    map.get(sortedStr).push(str);
-  }
-  return [...map.values()];
-}
+const head = (arr) => arr[0];
