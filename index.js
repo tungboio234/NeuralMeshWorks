b2e2bddeb83fd2779a1d6bcbb4075a51698040e7
tungboio234 <donandniconam1311@outlook.com>
2024-05-11 11:14:47 +0700

    aptos project

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
-const binarySearchIterative = (arr, target) => {
-  let left = 0;
-  let right = arr.length - 1;
-  while (left <= right) {
-    const mid = Math.floor((left + right) / 2);
-    if (arr[mid] === target) {
-      return mid;
-    } else if (arr[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
+function findTheDifference(s, t) {
+  let result = 0;
+  for (const char of s) {
+    result ^= char.charCodeAt(0);
   }
-  return -1;
-};
+  for (const char of t) {
+    result ^= char.charCodeAt(0);
+  }
+  return String.fromCharCode(result);
+}
