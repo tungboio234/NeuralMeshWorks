f6a6d5c2c5755cd388f86d287e11a48c549db004
tungboio234 <donandniconam1311@outlook.com>
2024-05-12 17:25:40 +0700

    update sdk task

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-function findTheDifference(s, t) {
-  let result = 0;
-  for (const char of s) {
-    result ^= char.charCodeAt(0);
+const binarySearchRecursive = (
+  arr,
+  target,
+  left = 0,
+  right = arr.length - 1,
+) => {
+  if (left > right) {
+    return -1;
   }
-  for (const char of t) {
-    result ^= char.charCodeAt(0);
+  const mid = Math.floor((left + right) / 2);
+  if (arr[mid] === target) {
+    return mid;
+  } else if (arr[mid] < target) {
+    return binarySearchRecursive(arr, target, mid + 1, right);
+  } else {
+    return binarySearchRecursive(arr, target, left, mid - 1);
   }
-  return String.fromCharCode(result);
-}
+};
