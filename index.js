55e0b5a05a4e6d674ca870c50b7ad76b6db24e2b
tungboio234 <donandniconam1311@outlook.com>
2024-05-13 13:57:26 +0700

    update task commit

--- index.js
+++ index.js
@@ -1,18 +1,3 @@
-const binarySearchRecursive = (
-  arr,
-  target,
-  left = 0,
-  right = arr.length - 1,
-) => {
-  if (left > right) {
-    return -1;
-  }
-  const mid = Math.floor((left + right) / 2);
-  if (arr[mid] === target) {
-    return mid;
-  } else if (arr[mid] < target) {
-    return binarySearchRecursive(arr, target, mid + 1, right);
-  } else {
-    return binarySearchRecursive(arr, target, left, mid - 1);
-  }
-};
+const message = "Hello world!";
+const messageLength = message.length;
+console.log(messageLength); // 13
