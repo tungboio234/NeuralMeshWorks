03a4f4b61e3b2b97d3b19f5dc928236b185f34f1
tungboio234 <donandniconam1311@outlook.com>
2024-05-15 12:59:26 +0700

    test task

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const message = "Hello world!";
-const messageLength = message.length;
-console.log(messageLength); // 13
+function canJump(nums) {
+  let maxJump = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (i > maxJump) return false;
+    maxJump = Math.max(maxJump, i + nums[i]);
+    if (maxJump >= nums.length - 1) return true;
+  }
+  return false;
+}
